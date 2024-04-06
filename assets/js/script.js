a034009bd6fc98e453ec202c9672fc3db42aa553
Aurus Soares <neiven131@gmail.com>
2024-04-06 12:51:17 -0300

    Alteração na forma como os erros de conexão são tratados

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,10 @@ const btnLoad = document.querySelector('#btnUpdate');
 const fetchDog = async (api_key) => {
     const data = await fetch(`https://api.thedogapi.com/v1/images/search?has_breeds=1&api_key=${api_key}`).then((response) => {
         if (!response.ok) {
-            throw Error(response.statusText);
+            throw new Error(response.status);
         }
         return response.json();
-    }).catch(e => console.log(e));
+    }).catch(e => console.log('Catch', e));
 
     return data;
 }
@@ -24,7 +24,7 @@ async function renderDog() {
         dogImage.onload = () => breedInfo.innerHTML = `<p><strong class="centro">${data['0']['breeds']['0']['name']}</strong></br>Tamanho: ${data['0']['breeds']['0']['height']['metric']} cm</br>Peso: ${data['0']['breeds']['0']['weight']['metric']} kg</br>Temperamento: ${data['0']['breeds']['0']['temperament']}</p>`
         btnLoad.classList.remove('rotate')
     } catch (error) {
-        breedInfo.innerHTML = `<p>Erro ao carregar as informações. Erro: ${error}`
+        breedInfo.innerHTML = '<p>Erro ao carregar as informações.'
     }
 
 }
